5cd431e97c021b08b50a043278d2c953e8ba6306
Jose <jose21gonzalez@gmail.com>
2017-05-09 15:38:40 -0500

    Bucles

---
+++ Javascript/Sentencias de Control/SDC.js
@@ -0,0 +1,44 @@
+/**
+ * Created by USRDEL on 9/5/17.
+ */
+
+
+var conHambre=[];
+
+if(conHambre.apellido){
+
+    console.log("Como")
+
+}else{
+
+    console.log("Trabjar")
+
+
+}
+var numero =null
+
+switch(numero){
+
+    case 0:
+
+        console.log("es 1")
+
+        break;
+
+    case 1:
+
+        console.log("es 2")
+
+
+        break;
+    default:
+
+        console.log("npi")
+
+
+
+
+
+
+
+}
