a875a2efa1ecbe73b90551ab3d7a56d0c5ebaa27
Jose <jose21gonzalez@gmail.com>
2017-05-09 16:29:09 -0500

    TempletaStrings

---
+++ Javascript/TempletaStrings/TemplateString.js
@@ -0,0 +1,14 @@
+/**
+ * Created by USRDEL on 9/5/17.
+ */
+var chao=`hiksa`
+
+var hola= `
+<h1>Hola</h1> ${chao}
+
+
+<p>Hola amigos de youtube</p>
+
+
+`
+hola
--- Javascript/app.js
+++ Javascript/app.js
@@ -4,3 +4,33 @@
 
 
 console.log('Hola mundo');
+
+
+var jose ={
+
+    nombre:"Jose",
+    apellido:"Gonzalez",
+    fechaNacimiento:new Date(),
+    imprimir: function () {
+
+        console.log(this.nombre,this.apellido)
+    },
+
+    año:function(){
+        return this.fechaNacimiento.getFullYear()
+    },
+
+    diferenciaEdad:function(edadAComparar){
+
+        return Math.abs(edadAComparar - this.fechaNacimiento.getFullYear())
+
+
+    }
+
+
+
+}
+
+jose.imprimir()
+console.log(jose.año())
+console.log(jose.diferenciaEdad(2013))
