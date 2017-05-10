12d30443c48e49e8b83e91f238232691a139d592
Jose <jose21gonzalez@gmail.com>
2017-05-10 14:53:31 -0500

    Operadores Finales

--- Javascript/TempletaStrings/TemplateString.js
+++ Javascript/TempletaStrings/TemplateString.js
@@ -130,3 +130,58 @@ let arreglos=[{
 
     }];
 
+
+
+
+//FIND Y FILTER
+//Operadores
+
+let arreglo = [8,10,7,9,6,4,5,3,6];
+
+let resultado=arreglo.map((valor,indice)=>{
+        return valor/5;
+
+})//filtra de acuerdo a un parametro que se le da
+.filter((valor)=>{
+    return valor>=1;
+
+})//encuentra la primera ocurrencia, si no encuentra nada devuelve undefined
+.find((valor)=>{
+    return valor ==1.7;
+});
+
+console.log(resultado);
+
+//Some
+let arreglo3 = [8,3,4,5,6,2,3,9]
+
+let resultado4= arreglo.map((valor,indice)=>{
+
+        return valor/5
+    }).some((valor)=>{
+
+    return valor<1
+})
+
+
+console.log(resultado4)
+
+
+
+//Every
+let arreglo3 = [8,3,4,5,6,2,3,9]
+
+let resultado4= arreglo.map((valor,indice)=>{
+
+        return valor/5
+    }).every((valor)=>{
+
+    return valor<1
+})
+
+
+console.log(resultado4)
+
+
+
+
