15bb047cd5a7be9d83e965084155e47399fb5a5d
Jose <jose21gonzalez@gmail.com>
2017-05-23 16:09:26 -0500

    html y css

--- web01/src/app/app.component.ts
+++ web01/src/app/app.component.ts
@@ -1,12 +1,57 @@
 import { Component } from '@angular/core';
 
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
+
 export class AppComponent {
-  title = 'Hail Pulpos!';
-  nombre:string = '\n Proximamente'
-  apellido:string= '\n!!!'
+  usuario:UsuarioInt = {
+    nombre:"",
+    apellido:"Gonzalez"
+  };
+  parrafo:string = "";
+
+  url:string="https://angular.io";
+  src:string="https://angular.io/resources/images/logos/angular/angular.png"
+
+
+
+
+  constructor(){
+    this.usuario.nombre = "Jose";
+    this.parrafo = "Mi mama me mima";
+
+    setTimeout(()=>{
+      this.usuario.nombre = "Jose"
+
+    },3000);
+  }
+
+  holaMundo(){
+    console.log("Hola mundo");
+  }
+
+
+
+  saludar(nombre:string,
+          apellido?:string):string{
+    return `Hola ${nombre} ${apellido}`;
+  }
+
+}
+
+
+
+
+interface UsuarioInt{
+  nombre:string,
+  apellido:string
 }
--- web01/src/app/app.module.ts
+++ web01/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { EstiloComponent } from './estilo/estilo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EstiloComponent
   ],
   imports: [
     BrowserModule,
